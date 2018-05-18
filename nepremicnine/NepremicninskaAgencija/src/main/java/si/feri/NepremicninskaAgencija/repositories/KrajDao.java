af77283e3ba386cdfeb3d81489ca858c7b987d28
DavidZele <david.zele@gmail.com>
2018-05-18 19:22:34 +0200

    Funkcija iskanja
    Iskanje IDjev za Kraje in Naslove

--- nepremicnine/NepremicninskaAgencija/src/main/java/si/feri/NepremicninskaAgencija/repositories/KrajDao.java
+++ nepremicnine/NepremicninskaAgencija/src/main/java/si/feri/NepremicninskaAgencija/repositories/KrajDao.java
@@ -12,7 +12,7 @@ public class KrajDao {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public int addKraj(String kraj, int postnaSt){
+    public int addKraj(String kraj, String postnaSt){
 
         String sql ="INSERT into Kraj(ImeKraja,PoštnaŠt) values(?,?)";
 
@@ -27,6 +27,17 @@ public class KrajDao {
             String kraj = (String)row.get("ImeKraja");
             int postnaSt = (int)row.get("PoštnaŠt");
             ret.add(new Kraj(kraj,postnaSt));
+        }
+        return ret;
+    }
+
+    public List<Integer> vrniID(String kraj, String postnaSt){
+        String sql = "SELECT Kraj.idKraj FROM Kraj WHERE ImeKraja=? AND PoštnaŠt=?";
+        List<Integer> ret = new ArrayList<Integer>();
+        List<Map<String,Object>> rows = jdbcTemplate.queryForList(sql, new Object[]{kraj,postnaSt});
+        for (Map row : rows) {
+            int id = (int)row.get("idKraj");
+            ret.add(id);
         }
         return ret;
     }
