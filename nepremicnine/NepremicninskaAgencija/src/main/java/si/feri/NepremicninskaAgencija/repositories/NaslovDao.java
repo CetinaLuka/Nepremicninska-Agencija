fe67713905100ee6108c448c93f2a48a5bf36ef3
KovacZan <zan.kovac1@student.um.si>
2018-05-30 19:28:43 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/EvaSmolak/Nepremicninska-Agencija""
    This reverts commit c002702c5434a43885c7a4c468023211d6853066.

--- nepremicnine/NepremicninskaAgencija/src/main/java/si/feri/NepremicninskaAgencija/repositories/NaslovDao.java
+++ nepremicnine/NepremicninskaAgencija/src/main/java/si/feri/NepremicninskaAgencija/repositories/NaslovDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import si.feri.NepremicninskaAgencija.models.Agent;
 import si.feri.NepremicninskaAgencija.models.Naslov;
 import si.feri.NepremicninskaAgencija.models.Nepremicnina;
+import si.feri.NepremicninskaAgencija.models.Podatki;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +43,23 @@ public class NaslovDao {
     public int vrniTKkraj(int id){
         String sql = "SELECT Kraj_idKraj FROM naslov WHERE idNaslov="+id;
         return Integer.parseInt((String)jdbcTemplate.queryForObject(sql, String.class));
+    }
+    public List<Podatki> vrniPodatke(){
+        String sql1 = "CREATE OR REPLACE VIEW zemljevid AS " +
+                "SELECT * FROM (nepremicnina LEFT JOIN naslov ON nepremicnina.tk_id_naslov=naslov.idNaslov " +
+                "LEFT JOIN kraj ON naslov.Kraj_idKraj=kraj.idKraj);";
+        jdbcTemplate.update(sql1, new Object[]{});
+        String sql=   "select tk_id_vrstaNepremicnine, ulica, hisnaSt, imeKraja from zemljevid" +
+                " group by tk_id_vrstaNepremicnine,ulica, hisnaSt, imeKraja;";
+        List<Podatki> ret = new ArrayList<Podatki>();
+        List<Map<String,Object>> rows = jdbcTemplate.queryForList(sql, new Object[]{});
+        for (Map row : rows) {
+            int tk_id_vrstaNepremicnine = (int) row.get("tk_id_vrstaNepremicnine");
+            String ulica = (String) row.get("ulica");
+            String hisnaSt = (String) row.get("hisnaSt");
+            String imeKraja = (String) row.get("imeKraja");
+            ret.add(new Podatki(tk_id_vrstaNepremicnine, ulica, hisnaSt, imeKraja));
+        }
+        return ret;
     }
 }
--- nepremicnine/NepremicninskaAgencija/src/main/webapp/WEB-INF/jsp/prijava.jsp
+++ nepremicnine/NepremicninskaAgencija/src/main/webapp/WEB-INF/jsp/prijava.jsp
@@ -111,7 +111,7 @@
                                </div>
                                     <div class="col-6 text-right">
                                      <a href="/registracija" >
-                                         <p>Želim se registrirat!</p>
+                                         <p>Želim se registrirati!</p>
                                      </a>
                                     </div>
                             </div>
