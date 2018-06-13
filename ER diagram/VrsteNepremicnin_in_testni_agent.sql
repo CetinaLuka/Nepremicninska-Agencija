2908c88ad7de2803985fec351756c252d7384c34
DavidZele <david.zele@gmail.com>
2018-06-13 10:17:15 +0200

    Popravljen ER

--- ER diagram/VrsteNepremicnin_in_testni_agent.sql
+++
@@ -1,5 +0,0 @@
-﻿insert into vrstanepremicnine (vrsta) values("Stanovanje");
-insert into vrstanepremicnine (vrsta) values("Hiša");
-insert into vrstanepremicnine (vrsta) values("Posest");
-
-insert into agent (ime,priimek,email,telefonskaSt,geslo) values ("Bartolomej","Modelić","nub@slab.si","041000000","jov");
